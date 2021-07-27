6ce125c1e8f6eb6faca13a25dce0a4b98f7c781c
Karim Idrisov <karim.idrisow2016@gmail.com>
2021-07-27 11:00:38 +1000

    feat: add visual pawns on board, and border to active fogure

--- js-games/chess/src/chess.js
+++ js-games/chess/src/chess.js
@@ -5,24 +5,48 @@ const BLACK = 2;
 class Board {
    constructor() {
       this.color = WHITE;
-      this.field = new Array(8).fill(new Array(8).fill(0));
+      this.field = new Array(8);
+      for (let i = 0; i < 8; i = i + 1) {
+         this.field[i] = new Array(8).fill(0)
+      }
+
+      this.whitePawns = new Array(8).fill(new Piece(WHITE, 'assets/img/pw.svg'));
    }
 
    getCurrentColor() {
       return this.color;
    }
 
-   getCell(row, col) {
-      return this.field[row][col];
+   getCell(num) {
+      return this.field[Math.floor(num / 8)][num % 8];
    }
 
+   drawBoard() {
+      const divs = document.querySelectorAll('.cell');
 
+      for (let i = 0; i < 8; i = i + 1) {
+         this.field[6][i] = this.whitePawns[i];
+         divs[6 * 8 + i].style.background = `url("${this.whitePawns[i].image}") 50% 50% / cover no-repeat`;
+         if (i % 2 === 0) {
+            divs[6 * 8 + i].style.backgroundColor = "#fff";
+         }
+      }
+
+      divs.forEach(cell => cell.addEventListener("click", (event) => {
+         divs.forEach(div => div.classList.remove('active'))
+
+         if (this.getCell(event.target.getAttribute('data-id')) !== 0) {
+            event.target.classList.add('active');
+         }
+      }))
+   }
 }
 
 // basic Piece
 class Piece {
-   constructor(color) {
+   constructor(color, src) {
       this.color = color;
+      this.image = src;
    }
 
    getColor() {
@@ -32,10 +56,7 @@ class Piece {
 }
 
 class Pawn extends Piece {
-   constructor(color) {
-      super(color);
+   constructor(color, src) {
+      super(color, src);
    }
 }
-
-const board = new Board();
-console.log(board.field);
